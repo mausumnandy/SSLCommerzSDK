9b6ab18c129f0c1d51a352c1a3d8e3c25d63f823
K.M. Imrul Kayes Sikdar <imrul.kayes@sslwireless.com>
2024-11-24 12:48:43 +0600

    SPM support added for v2.0.5

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SSLCommerzSDK",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SSLCommerzSDK",
+            targets: ["SSLCommerzSDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SSLCommerzSDK",
+            path: "./Framework/SSLCommerzSDK.xcframework"  // Correct path to XCFramework
+        ),
+    ]
+)
